1b7868b15e1d3e024bd37864560736bfd7c5fff4
Hiroshi Kori <hiroshi.maybe@gmail.com>
2014-05-03 15:18:42 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var sys = require( "sys" );
+var http = require( "http" );
+ 
+// Create an HTTP server.
+var server = http.createServer(
+function(request, response) {
+  response.writeHead(200, {"content-type": "text/plain"});
+  response.write("Hellow world!\n");
+  response.end();
+});
+ 
+// Listen to port 8080
+server.listen(8080);
+
+// Logging 
+sys.puts( "Server is running on 8080" );
