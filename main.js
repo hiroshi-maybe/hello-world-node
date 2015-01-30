fcaf829d6441a6a4612ab9285f94632937a3d7d9
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-01-29 23:22:02 -0800

    fix line break

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ var sys = require( "sys" );
 var http = require( "http" );
  
 // Create an HTTP server.
-var server = http.createServer(
-function(request, response) {
+var server = http.createServer(function(request, response) {
   response.writeHead(200, {"content-type": "text/plain"});
   response.write("Hellow world!\n");
   response.end();
